1946d802291964281fb4aa4ccc68010630eb666d
Mauricio-mty <00000422@uca.edu.sv>
2025-12-23 10:00:49 -0600

    configuration route fixed

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 module.exports = cors({
-    origin: ['http://localhost:5173','http://192.168.1.5:5173'],
+    origin: ['http://127.0.0.1:5173','http://192.168.1.5:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
